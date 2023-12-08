7248a5735c38dad06534ab6b5e72ce22116e0a22
XOP <wdybih@gmail.com>
2023-12-08 13:13:40 +0100

    generator scaffolding

---
+++ turbo/generators/config.ts
@@ -0,0 +1,60 @@
+import { PlopTypes } from '@turbo/gen';
+
+const CATEGORIES = [
+  'Form',
+  'Navigation',
+  'Feedback',
+  'Content',
+  'Status',
+  'Data',
+  'Overlay',
+  'Date & Time',
+];
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setGenerator('awsm-ui-component', {
+    description: 'Adds a new UI Component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the NAME of the component?',
+      },
+      {
+        type: 'list',
+        name: 'category',
+        message: 'What is the CATEGORY of the component?',
+        choices: CATEGORIES,
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/ui-component-core.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.css.ts',
+        templateFile: 'templates/ui-component-css.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/ui-component-stories.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.ts',
+        template:
+          "export { default as {{pascalCase name}} } from './{{pascalCase name}}';",
+      },
+      {
+        type: 'append',
+        path: 'src/components/index.ts',
+        template:
+          "export { {{pascalCase name}} } from './{{pascalCase name}}';",
+      },
+    ],
+  });
+}
