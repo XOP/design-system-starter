4a9023d034e00e4d9871ccd1335b35de01460c3b
XOP <wdybih@gmail.com>
2023-12-26 11:29:02 +0100

    icons build update

--- packages/awsm-icons/tsup.config.ts
+++ packages/awsm-icons/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['lib/icons/*'],
-  outDir: 'dist/lib/icons',
+  outDir: 'dist',
   treeshake: true,
   external: ['react', 'react-dom'],
   splitting: false,
