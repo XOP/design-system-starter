a8962476f5397e04a34c3dd9508d88930af484c9
XOP <wdybih@gmail.com>
2024-03-19 19:08:52 +0100

    switch tests

---
+++ packages/awsm-ui/src/components/Switch/Switch.spec.tsx
@@ -0,0 +1,77 @@
+import { it, describe, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import { Switch } from '.';
+import { styles, indicatorStyles } from './Switch.css';
+
+describe('Switch', () => {
+  it('should render component with default props', () => {
+    const { container } = render(<Switch />);
+
+    const switchComponent = container.firstChild;
+
+    expect(switchComponent).toBeInTheDocument();
+    expect(container.querySelector('input')).toBeInTheDocument();
+  });
+
+  it('should render component with a custom label', () => {
+    const { getByText } = render(<Switch>Label</Switch>);
+    const switchComponent = getByText('Label');
+
+    expect(switchComponent).toBeInTheDocument();
+  });
+
+  it('should apply color from props', () => {
+    const { container } = render(<Switch color="secondary">Label</Switch>);
+
+    const switchIndicator = container.getElementsByClassName(
+      indicatorStyles({}),
+    )[0];
+
+    expect(switchIndicator).toBeInTheDocument();
+    expect(switchIndicator).toHaveClass(
+      indicatorStyles({ color: 'secondary' }),
+    );
+  });
+
+  it('should apply the styles according to props', () => {
+    const { container } = render(
+      <Switch color="secondary" size="small">
+        Label
+      </Switch>,
+    );
+
+    const switchComponent = container.firstChild;
+
+    expect(switchComponent).toHaveClass(styles({ size: 'small' }));
+
+    const switchIndicator = container.getElementsByClassName(
+      indicatorStyles({}),
+    )[0];
+
+    expect(switchIndicator).toBeInTheDocument();
+    expect(switchIndicator).toHaveClass(
+      indicatorStyles({ color: 'secondary' }),
+    );
+  });
+
+  it('should render selected', () => {
+    const { container } = render(<Switch isSelected />);
+
+    const switchComponent = container.firstChild;
+    const switchInput = container.querySelector('input');
+
+    expect(switchComponent).toHaveAttribute('data-selected', 'true');
+    expect(switchInput).toBeChecked();
+  });
+
+  it('should render in disabled state', () => {
+    const { container } = render(<Switch isDisabled />);
+
+    const switchComponent = container.firstChild;
+    const switchInput = container.querySelector('input');
+
+    expect(switchComponent).toHaveAttribute('data-disabled', 'true');
+    expect(switchInput).toBeDisabled();
+  });
+});
